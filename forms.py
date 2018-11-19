c13fd4cc4b3786240d8a7643a2669564eadce0df
yugaljain1999 <32617984+yugaljain1999@users.noreply.github.com>
2018-11-19 21:15:20 +0530

    update forms.py
    importing one more  function(IntegerField) from wtforms

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,IntegerField
 
 class Addform(FlaskForm):
     name=StringField("enter name of Puppy")
     submit=SubmitField("Submit")
 class Delform(FlaskForm):
-    id=StringField("Enter id which you want to delete")
+    id=IntegerField("Enter id which you want to delete")
     submit=SubmitField("Submit")
